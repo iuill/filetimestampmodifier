bad6828a1c06346164efe298156beba07fd50252
iuill <jhdkoiiondt@yahoo.co.jp>
2022-07-09 11:38:39 +0900

    add: file

---
+++ src/FileTimeStampModifier/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace FileTimeStampModifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDirPath = new System.Windows.Forms.TextBox();
+            this.buttonOpenDirDialog = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonRenewFileTime = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonReload = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "フォルダ";
+            // 
+            // textBoxDirPath
+            // 
+            this.textBoxDirPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDirPath.Location = new System.Drawing.Point(51, 8);
+            this.textBoxDirPath.Name = "textBoxDirPath";
+            this.textBoxDirPath.Size = new System.Drawing.Size(579, 23);
+            this.textBoxDirPath.TabIndex = 1;
+            // 
+            // buttonOpenDirDialog
+            // 
+            this.buttonOpenDirDialog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpenDirDialog.Location = new System.Drawing.Point(636, 7);
+            this.buttonOpenDirDialog.Name = "buttonOpenDirDialog";
+            this.buttonOpenDirDialog.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenDirDialog.TabIndex = 2;
+            this.buttonOpenDirDialog.Text = "フォルダ選択";
+            this.buttonOpenDirDialog.UseVisualStyleBackColor = true;
+            this.buttonOpenDirDialog.Click += new System.EventHandler(this.buttonOpenDirDialog_Click);
+            // 
+            // buttonRenewFileTime
+            // 
+            this.buttonRenewFileTime.Location = new System.Drawing.Point(219, 6);
+            this.buttonRenewFileTime.Name = "buttonRenewFileTime";
+            this.buttonRenewFileTime.Size = new System.Drawing.Size(75, 23);
+            this.buttonRenewFileTime.TabIndex = 4;
+            this.buttonRenewFileTime.Text = "更新";
+            this.buttonRenewFileTime.UseVisualStyleBackColor = true;
+            this.buttonRenewFileTime.Click += new System.EventHandler(this.buttonRenewFileTime_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(724, 413);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ファイル名";
+            this.columnHeader1.Width = 400;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "数値";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "更新日時";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "作成日時";
+            this.columnHeader4.Width = 120;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxDirPath);
+            this.panel1.Controls.Add(this.buttonOpenDirDialog);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(724, 36);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.buttonReload);
+            this.panel2.Controls.Add(this.buttonRenewFileTime);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 449);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(724, 36);
+            this.panel2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(411, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(300, 15);
+            this.label3.TabIndex = 7;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "基準時刻";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd hh:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(73, 6);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(140, 23);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // buttonReload
+            // 
+            this.buttonReload.Location = new System.Drawing.Point(315, 6);
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.Size = new System.Drawing.Size(75, 23);
+            this.buttonReload.TabIndex = 4;
+            this.buttonReload.Text = "再読み込み";
+            this.buttonReload.UseVisualStyleBackColor = true;
+            this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listView1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 36);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(724, 413);
+            this.panel3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 485);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "タイムスタンプ更新ツール";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBoxDirPath;
+        private Button buttonOpenDirDialog;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button buttonRenewFileTime;
+        private ListView listView1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private DateTimePicker dateTimePicker1;
+        private Label label2;
+        private Label label3;
+        private Button buttonReload;
+    }
+}
---
+++ src/FileTimeStampModifier/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FileTimeStampModifier
+{
+    public partial class Form1 : Form
+    {
+        FileTimeStampModifier.ListViewItemComparer sorter = new FileTimeStampModifier.ListViewItemComparer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.sorter.ColumnModes = new ListViewItemComparer.ComparerMode[]
+            {
+                ListViewItemComparer.ComparerMode.String,
+                ListViewItemComparer.ComparerMode.Integer,
+                ListViewItemComparer.ComparerMode.DateTime,
+                ListViewItemComparer.ComparerMode.DateTime,
+            };
+            this.listView1.ListViewItemSorter = this.sorter;
+        }
+
+        private void buttonOpenDirDialog_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == this.folderBrowserDialog1.ShowDialog())
+            {
+                this.textBoxDirPath.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+
+            this.loadFileList(this.textBoxDirPath.Text);
+        }
+
+        private void loadFileList(string path)
+        {
+            this.listView1.Items.Clear();
+
+            var regex = new Regex(@"^\D*(?<num>\d+)\D*$", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+            var regexAsciiNum = new Regex(@"^[0-9]+$", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+            var map = new Dictionary<char, char>();
+            map.Add('０', '0');
+            map.Add('１', '1');
+            map.Add('２', '2');
+            map.Add('３', '3');
+            map.Add('４', '4');
+            map.Add('５', '5');
+            map.Add('６', '6');
+            map.Add('７', '7');
+            map.Add('８', '8');
+            map.Add('９', '9');
+
+            var files = Directory.GetFiles(path);
+            foreach (var item in files)
+            {
+                var fi = new FileInfo(item);
+                var m = regex.Match(fi.Name);
+
+                var num = 0;
+                if (m.Success)
+                {
+                    var tmp = m.Groups["num"].Value.Select(x =>
+                    {
+                        if (regexAsciiNum.IsMatch(x.ToString()))
+                        {
+                            return x;
+                        }
+                        else
+                        {
+                            return map[x];
+                        }
+                    }).ToList();
+                    num = int.Parse(string.Join("", tmp));
+                }
+
+                var format = "yyyy/MM/dd hh:mm:ss";
+                this.listView1.Items.Add(new ListViewItem(new string[] { fi.Name, num.ToString(), fi.LastWriteTime.ToString(format), fi.CreationTime.ToString(format) }));
+            }
+
+
+            this.sorter.Column = 1;
+            this.listView1.Sort();
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            this.sorter.Column = e.Column;
+            this.listView1.Sort();
+        }
+
+        private void buttonRenewFileTime_Click(object sender, EventArgs e)
+        {
+            this.label3.Text = "START: " + DateTime.Now.ToString("hh:mm:ss");
+
+            DateTime datetime = this.dateTimePicker1.Value;
+            foreach (ListViewItem item in this.listView1.Items)
+            {
+                var fi = new FileInfo(Path.Combine(this.textBoxDirPath.Text, item.SubItems[0].Text));
+                fi.LastWriteTime = datetime;
+                fi.CreationTime = datetime;
+
+                datetime = datetime.AddSeconds(1);
+            }
+
+            this.label3.Text = "FINISHED: " + DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void buttonReload_Click(object sender, EventArgs e)
+        {
+            var path = this.textBoxDirPath.Text;
+            if (!string.IsNullOrEmpty(path) && Directory.Exists(path))
+            {
+                // orderが変わってしまうが、あまり困らないので放置する
+                this.loadFileList(this.textBoxDirPath.Text);
+            }
+            else
+            {
+                this.label3.Text = "無効なディレクトリです";
+            }
+        }
+    }
+}
